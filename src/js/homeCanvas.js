ac7ab5e066aba1164ed637724c096e682e992982
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2021-02-18 22:37:46 +0600

    finished making parallax

--- src/js/homeCanvas.js
+++ src/js/homeCanvas.js
@@ -8,26 +8,46 @@ class canvasParallax {
       circle: {
         x: 0,
         y: 0,
-        radius: 40,
+        src: null,
+        width: 80,
+        startingPos: {
+          x: 0,
+          y: 0,
+        },
       },
       triangle: {
         x: 0,
         y: 0,
-        hypo: 60,
+        src: null,
+        width: 100,
+        startingPos: {
+          x: 0,
+          y: 0,
+        },
       },
       line: {
         x: 0,
         y: 0,
+        src: null,
+        width: 360,
+        startingPos: {
+          x: 0,
+          y: 0,
+        },
       },
     };
     this.scale = 3;
     this.color = "#BD00FF";
     this.sectionPadding = 0;
+    this.savedMousePos = {
+      x: 0,
+      y: 0,
+    };
   }
 
   init() {
     this.setCanvas();
-    this.setElementsPos();
+    this.setElements();
 
     if (this.ctx) {
       this.draw();
@@ -49,87 +69,155 @@ class canvasParallax {
     cnvParent.appendChild(this.cnv);
   }
 
-  setElementsPos() {
-    let header1 = document.querySelector("#home h1").getBoundingClientRect();
-    let header2 = document.querySelector("#home h3").getBoundingClientRect();
-    let header3 = document
-      .querySelector("#home header")
-      .getBoundingClientRect();
+  setElements() {
+    this.setCircle();
+    this.setLine();
+    this.setTriangle();
+  }
 
-    this.elements.circle.x = Math.floor(header1.x - this.sectionPadding);
-    this.elements.circle.y = Math.floor(header1.y);
+  setCircle() {
+    let header = document.querySelector("#home h1").getBoundingClientRect();
 
-    this.elements.triangle.x = Math.floor(
-      header2.x + header2.width - this.sectionPadding / 1.2
-    );
-    this.elements.triangle.y = Math.floor(header2.y + 16);
+    this.elements.circle.src = document.querySelector("#ellipse");
+    this.elements.circle.x = Math.floor(header.x - this.sectionPadding);
+    this.elements.circle.y = Math.floor(header.y);
+    this.elements.circle.startingPos = {
+      x: this.elements.circle.x,
+      y: this.elements.circle.y,
+    };
+  }
 
+  setLine() {
+    let header = document.querySelector("#home header").getBoundingClientRect();
+
+    this.elements.line.src = document.querySelector("#line");
     this.elements.line.x = Math.floor(this.cnvWidth / 2);
-    this.elements.line.y = Math.floor(header3.y + header3.height / 2);
+    this.elements.line.y = Math.floor(header.y + header.height / 2);
+    this.elements.line.startingPos = {
+      x: this.elements.line.x,
+      y: this.elements.line.y,
+    };
+  }
+
+  setTriangle() {
+    let header = document.querySelector("#home h3").getBoundingClientRect();
+
+    this.elements.triangle.src = document.querySelector("#triangle");
+    this.elements.triangle.x = Math.floor(
+      header.x + header.width - this.sectionPadding / 1.2
+    );
+    this.elements.triangle.y = Math.floor(header.y + 16);
+    this.elements.triangle.startingPos = {
+      x: this.elements.triangle.x,
+      y: this.elements.triangle.y,
+    };
   }
 
   draw() {
+    this.cleanCanvas();
     this.drawElements();
   }
 
   drawElements() {
-    this.ctx.strokeStyle = this.color;
-
-    this.drawLine();
-    this.drawTriangle();
     this.drawCircle();
-  }
-
-  drawLine() {
-    let line = this.elements.line;
-
-    this.ctx.beginPath();
-    this.ctx.moveTo(line.x + 100, line.y - 120);
-    this.ctx.lineTo(line.x - 120, line.y + 100);
-    this.ctx.stroke();
+    this.drawTriangle();
+    this.drawLine();
   }
 
   drawCircle() {
     let circle = this.elements.circle;
-
-    this.ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, false);
-    this.ctx.stroke();
+    this.ctx.drawImage(
+      circle.src,
+      circle.x - circle.width / 2,
+      circle.y - circle.width / 2,
+      circle.width,
+      circle.width
+    );
   }
 
   drawTriangle() {
-    let circle = this.elements.circle;
     let triangle = this.elements.triangle;
-    let secondPos = {
-      x: Math.floor(triangle.x - triangle.hypo),
-      y: Math.floor(
-        triangle.y -
-          Math.sqrt(
-            triangle.hypo ** 2 -
-              (triangle.hypo * Math.cos(this.degToRad(28))) ** 2
-          )
-      ),
-    };
-
-    this.ctx.beginPath();
-    this.ctx.moveTo(triangle.x, triangle.y);
-    this.ctx.lineTo(secondPos.x, secondPos.y);
-    this.ctx.lineTo(
-      Math.floor(
-        secondPos.x +
-          Math.sqrt(
-            triangle.hypo ** 2 -
-              (triangle.hypo * Math.sin(this.degToRad(86))) ** 2
-          )
-      ),
-      Math.floor(secondPos.y + triangle.hypo)
+    this.ctx.drawImage(
+      triangle.src,
+      triangle.x - triangle.width / 1.5,
+      triangle.y - triangle.width / 2,
+      80,
+      80
     );
-    this.ctx.lineTo(triangle.x, triangle.y);
-    this.ctx.moveTo(circle.x + circle.radius, circle.y);
-    this.ctx.stroke();
   }
 
-  degToRad(deg) {
-    return (deg * Math.PI) / 180;
+  drawLine() {
+    let line = this.elements.line;
+    this.ctx.drawImage(
+      line.src,
+      line.x - line.width / 2,
+      line.y - line.width / 2,
+      line.width,
+      line.width
+    );
+  }
+
+  cleanCanvas() {
+    this.ctx.clearRect(0, 0, this.cnvWidth, this.cnvHeight);
+  }
+
+  getMousePos(x, y) {
+    this.checkingMousePos(x, y);
+  }
+
+  getMouseLeaveEvent() {
+    let triangle = this.elements.triangle;
+    let circle = this.elements.circle;
+    let line = this.elements.line;
+
+    triangle.x = triangle.startingPos.x;
+    triangle.y = triangle.startingPos.y;
+    circle.x = circle.startingPos.x;
+    circle.y = circle.startingPos.y;
+    line.x = line.startingPos.x;
+    line.y = line.startingPos.y;
+
+    console.log("mouse leaved");
+    this.update();
+  }
+
+  checkingMousePos(x, y) {
+    let checking = {
+      x: false,
+      y: false,
+    };
+    let cnvPos = this.cnv.getBoundingClientRect();
+
+    if (x > cnvPos.x || x < cnvPos.x + cnvPos.cnvWidth) {
+      checking.x = true;
+    }
+    if (y > cnvPos.y || y < cnvPos.y + cnvPos.cnvHeight) {
+      checking.y = true;
+    }
+
+    if (checking.x && checking.y) {
+      this.updateElementsPos(x, y);
+    }
+  }
+
+  updateElementsPos(x, y) {
+    let triangle = this.elements.triangle;
+    let circle = this.elements.circle;
+    let line = this.elements.line;
+    let speed = 40;
+
+    triangle.x = triangle.startingPos.x + x / speed;
+    triangle.y = triangle.startingPos.y + y / speed;
+    circle.x = circle.startingPos.x + x / speed;
+    circle.y = circle.startingPos.y + y / speed;
+    line.x = line.startingPos.x + x / speed;
+    line.y = line.startingPos.y + y / speed;
+
+    this.update();
+  }
+
+  update() {
+    window.requestAnimationFrame(() => this.draw());
   }
 }
 
@@ -138,3 +226,11 @@ let homeCanvas = new canvasParallax();
 window.addEventListener("load", () => {
   homeCanvas.init();
 });
+
+document.querySelector("#home").addEventListener("mousemove", (e) => {
+  homeCanvas.getMousePos(e.clientX, e.clientY);
+});
+
+// document.querySelector("#home").addEventListener("mouseleave", () => {
+//   homeCanvas.getMouseLeaveEvent();
+// });
